504e3603004d00cb94d1c410c8a730878cf57153
Avinash Reddy <havinashreddy123@gmail.com>
2026-04-19 07:51:42 +0530

    simplearthmitecoperators

---
+++ sampleprogram.js
@@ -0,0 +1,14 @@
+// Arthimetic operators +,-,*,**,/,%..
+let a=10,b=20,result=0;
+result=a+b; //10+20=30
+result=a-b; //10-20=-10
+result=a*b; //10*20=200
+result=a**b;//10^20=100000000000000000000
+result=a/b; //10/20=0.5  
+result=a%b; //10%20=10
+console.log("---addition",a+b)
+console.log("---Subtraction",a-b)
+console.log("---Multiplication",a*b)
+console.log("---a power b",a**b)
+console.log("---division",a/b)
+console.log("---modulus",a%b)
